9fed6045851759f34c381414d62aff8807899d39
Suyashtrent <141958076+suyashstr@users.noreply.github.com>
2023-09-09 14:09:36 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,49 @@
+// header of js
+let header = document.querySelector('header');
+
+window.addEventListener('scroll',() => {
+    header,classList.toggle('shadow',window.scrollY > 0);
+});
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+
+window.onscroll = () => {
+    menu.classList.remove('bx-x');
+    navbar.classList.remove('active');
+}
+
+const sr = ScrollReveal({
+     origin: 'top',
+     distance:'60px',
+     duration: 2500,
+     delay:400,
+
+})
+ 
+sr.reveal('.home-text,.buds-text',{origin: 'left'})
+sr.reveal('.home-img,.buds-img',{origin: 'right'})
+sr.reveal('.heading',{delay: 200})
+sr.reveal('.specs-details .box' ,{origin: 'left', interval:200})
+sr.reveal('.specs-img',{delay: 600})
+sr.reveal('.shop-container .box,.footer .logo,.footer .footer-box',{interval:150})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
